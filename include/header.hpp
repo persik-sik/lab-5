3f91195d034093cee1a189b9986865fbbed1ac28
Nikita Volkov <rtyuehe12321@yandex.ru>
2024-11-02 12:18:52 +0300

    Create header.hpp

---
+++ include/header.hpp
@@ -0,0 +1,7 @@
+void SortByName(std::vector<Student>&);
+void SortByRating(std::vector<Student>&);
+size_t CountTwoness(const std::vector<Student>&);
+size_t CountExcellent(const std::vector<Student>&);
+std::vector<Student> VectorMathExcellent(const std::vector<Student>&);
+std::vector<std::string> GroupsId(const std::vector<Student>&);
+std::vector<Group> Groups(const std::vector<Student>&);
